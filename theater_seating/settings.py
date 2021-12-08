b3fe5b1cace30f2ca9e1ac300f49509ebc31e93d
mohammad <mohammad_hasani@live.com>
2021-12-08 15:18:56 +0330

    change allow hosts

--- theater_seating/settings.py
+++ theater_seating/settings.py
@@ -29,7 +29,7 @@ AUTH_USER_MODEL = 'user.User'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1', '185.235.40.196', '192.168.100.101']
+ALLOWED_HOSTS = ['*',]
 
 # Application definition
 
